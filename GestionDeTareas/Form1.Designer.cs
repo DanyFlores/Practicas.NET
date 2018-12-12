a6102b1d57515e0e79d14241a386b25edc9d9470
Creativa-PC <lopezfloresjuandaniel123@gmail.com>
2018-12-12 00:26:16 -0600

    daniel

--- GestionDeTareas/Form1.Designer.cs
+++ GestionDeTareas/Form1.Designer.cs
@@ -28,13 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ltb_Task = new System.Windows.Forms.ListBox();
+            this.txt_Task = new System.Windows.Forms.TextBox();
+            this.lbl_Tarea = new System.Windows.Forms.Label();
+            this.btn_AddTask = new System.Windows.Forms.Button();
+            this.btn_Guardar = new System.Windows.Forms.Button();
+            this.btn_Eliminar = new System.Windows.Forms.Button();
+            this.btn_Cancelar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Controls.Add(this.btn_Cancelar);
+            this.panel1.Controls.Add(this.btn_Eliminar);
+            this.panel1.Controls.Add(this.btn_Guardar);
+            this.panel1.Controls.Add(this.btn_AddTask);
+            this.panel1.Controls.Add(this.lbl_Tarea);
+            this.panel1.Controls.Add(this.txt_Task);
+            this.panel1.Controls.Add(this.ltb_Task);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(643, 313);
+            this.panel1.TabIndex = 0;
+            // 
+            // ltb_Task
+            // 
+            this.ltb_Task.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ltb_Task.FormattingEnabled = true;
+            this.ltb_Task.Location = new System.Drawing.Point(5, 108);
+            this.ltb_Task.Name = "ltb_Task";
+            this.ltb_Task.Size = new System.Drawing.Size(634, 199);
+            this.ltb_Task.TabIndex = 0;
+            // 
+            // txt_Task
+            // 
+            this.txt_Task.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_Task.Location = new System.Drawing.Point(54, 58);
+            this.txt_Task.Multiline = true;
+            this.txt_Task.Name = "txt_Task";
+            this.txt_Task.Size = new System.Drawing.Size(507, 33);
+            this.txt_Task.TabIndex = 1;
+            // 
+            // lbl_Tarea
+            // 
+            this.lbl_Tarea.AutoSize = true;
+            this.lbl_Tarea.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Tarea.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Tarea.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_Tarea.Location = new System.Drawing.Point(3, 59);
+            this.lbl_Tarea.Name = "lbl_Tarea";
+            this.lbl_Tarea.Size = new System.Drawing.Size(45, 16);
+            this.lbl_Tarea.TabIndex = 2;
+            this.lbl_Tarea.Text = "Tarea";
+            // 
+            // btn_AddTask
+            // 
+            this.btn_AddTask.Location = new System.Drawing.Point(6, 6);
+            this.btn_AddTask.Name = "btn_AddTask";
+            this.btn_AddTask.Size = new System.Drawing.Size(124, 33);
+            this.btn_AddTask.TabIndex = 3;
+            this.btn_AddTask.Text = "Agregar Nueva Tarea";
+            this.btn_AddTask.UseVisualStyleBackColor = true;
+            this.btn_AddTask.Click += new System.EventHandler(this.btn_AddTask_Click);
+            // 
+            // btn_Guardar
+            // 
+            this.btn_Guardar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Guardar.Location = new System.Drawing.Point(324, 6);
+            this.btn_Guardar.Name = "btn_Guardar";
+            this.btn_Guardar.Size = new System.Drawing.Size(75, 33);
+            this.btn_Guardar.TabIndex = 4;
+            this.btn_Guardar.Text = "Guardar";
+            this.btn_Guardar.UseVisualStyleBackColor = true;
+            this.btn_Guardar.Click += new System.EventHandler(this.btn_Guardar_Click);
+            // 
+            // btn_Eliminar
+            // 
+            this.btn_Eliminar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Eliminar.Location = new System.Drawing.Point(405, 6);
+            this.btn_Eliminar.Name = "btn_Eliminar";
+            this.btn_Eliminar.Size = new System.Drawing.Size(75, 33);
+            this.btn_Eliminar.TabIndex = 5;
+            this.btn_Eliminar.Text = "Eliminar";
+            this.btn_Eliminar.UseVisualStyleBackColor = true;
+            this.btn_Eliminar.Click += new System.EventHandler(this.btn_Eliminar_Click);
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Cancelar.Location = new System.Drawing.Point(486, 6);
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.Size = new System.Drawing.Size(75, 33);
+            this.btn_Cancelar.TabIndex = 6;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.UseVisualStyleBackColor = true;
+            this.btn_Cancelar.Click += new System.EventHandler(this.btn_Cancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(643, 313);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_AddTask;
+        private System.Windows.Forms.Label lbl_Tarea;
+        private System.Windows.Forms.TextBox txt_Task;
+        private System.Windows.Forms.ListBox ltb_Task;
+        private System.Windows.Forms.Button btn_Cancelar;
+        private System.Windows.Forms.Button btn_Eliminar;
+        private System.Windows.Forms.Button btn_Guardar;
     }
 }
 
--- GestionDeTareas/Form1.cs
+++ GestionDeTareas/Form1.cs
@@ -16,5 +16,57 @@ namespace GestionDeTareas
         {
             InitializeComponent();
         }
+        #region Metodos
+        public void AddNewTask()
+        {
+            this.txt_Task.Enabled = true;
+            this.btn_Guardar.Enabled = true;
+            this.btn_Cancelar.Enabled = true;
+            this.txt_Task.Focus();
+        }
+        public void DeleteTask()
+        {
+
+        }
+        public void SaveChanges()
+        {
+            this.ltb_Task.Items.Add(txt_Task.Text);
+            this.ltb_Task.Enabled = true;
+        }
+        public void Cancel()
+        {
+
+        }
+        #endregion
+
+        #region Eventos
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.btn_Guardar.Enabled = false;
+            this.btn_Eliminar.Enabled = false;
+            this.btn_Cancelar.Enabled = false;
+            this.ltb_Task.Enabled = false;
+            this.txt_Task.Enabled = false;
+        }
+        private void btn_AddTask_Click(object sender, EventArgs e)
+        {
+            this.AddNewTask();
+        }
+        private void btn_Guardar_Click(object sender, EventArgs e)
+        {
+            this.SaveChanges();
+        }
+
+        private void btn_Eliminar_Click(object sender, EventArgs e)
+        {
+            this.DeleteTask();
+        }
+
+        private void btn_Cancelar_Click(object sender, EventArgs e)
+        {
+            this.Cancel();
+        }
+        #endregion
+
     }
 }
